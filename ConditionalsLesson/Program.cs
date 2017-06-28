ce514af2fd60bda8cdc9b1bb96e2abfefada9ca1
Adam King <adam81king@gmail.com>
2017-06-28 13:45:10 -0400

    Updated Conditionals

--- ConditionalsLesson/Program.cs
+++ ConditionalsLesson/Program.cs
@@ -9,7 +9,13 @@ namespace ConditionalsLesson
     class Program
     {
         static void Main(string[] args)
-        {
+        {       Console.WriteLine("What Is the total bill?");
+            int totalBill = int.Parse(Console.ReadLine());
+                if (totalBill >= 50)
+                Console.WriteLine("You Get A 10% Discount!");
+
+            if (totalBill < 50)
+                Console.WriteLine("You Get A 5% Discount!");
         }
     }
 }
